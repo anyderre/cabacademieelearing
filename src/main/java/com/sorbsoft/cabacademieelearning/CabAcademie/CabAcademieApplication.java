90b1a1d00ca58973a5e6519d40000d0ecf278130
Dany <ridoreda1992@gmail.com>
2018-05-13 10:53:28 -0400

    first commit

---
+++ src/main/java/com/sorbsoft/cabacademieelearning/CabAcademie/CabAcademieApplication.java
@@ -0,0 +1,12 @@
+package com.sorbsoft.cabacademieelearning.CabAcademie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabAcademieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabAcademieApplication.class, args);
+	}
+}
